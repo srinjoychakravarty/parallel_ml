43b174ebabbcc9260a28a3018f0d95d0ef92a0c4
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2020-07-09 19:08:54 -0400

    :guardsman: question 2 code working

--- part2.py
+++ part2.py
@@ -3,50 +3,57 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
-import time
-from matplotlib import pyplot
+import matplotlib
 
-data = read_csv('train.csv')	# load data
+from matplotlib import pyplot
+import numpy
+import time
+
+data = read_csv('train.csv')
 dataset = data.values
 
-X = dataset[:,0:94]	# split data into X and y
-y = dataset[:,94]
-label_encoded_y = LabelEncoder().fit_transform(y)	# encode string class values as integers
-n_estimators = [100, 200, 300, 400, 500]
+X = dataset[:5000, 0:94]   # split data into X and y and sampling from total row entries to save time
+y = dataset[:5000, 94]     
+
+label_encoded_y = LabelEncoder().fit_transform(y) 
+
+model = XGBClassifier()
+# number_of_trees = [100, 200, 300, 400, 500]
+number_of_trees = [35, 40, 45, 50, 55]
 max_depth = [1]
 subsample = [1.0]
 colsample_bytree = [0.18]
-learning_rate = [0.1, 0.01, 0.001, 0.0001]
+learning_rate = [0.0001, 0.001, 0.01, 0.1]
 gamma = [0] 
 objective = ['binary:logistic']
 reg_alpha = [0.2]
 reg_lambda = [0.4]
+num_threads = [8, 7, 6, 5, 4, 3, 2, 1]
+param_grid = {'reg_lambda': reg_lambda,'reg_alpha': reg_alpha, 'objective': objective, 'gamma': gamma, 'colsample_bytree': colsample_bytree, 'subsample': subsample, 'max_depth': max_depth, 'n_estimators': number_of_trees, 'learning_rate': learning_rate}
+kfold = StratifiedKFold(n_splits = 10, shuffle = True, random_state = 93)
 
-param_grid = {'reg_lambda': reg_lambda,'reg_alpha': reg_alpha, 'objective': objective, 'gamma': gamma, 'colsample_bytree': colsample_bytree, 'subsample': subsample, 'max_depth': max_depth, 'n_estimators': n_estimators, 'learning_rate': learning_rate}
-
-kfold_cross_validation = StratifiedKFold(n_splits = 10)
-
-# results = []
-num_threads = [2, 1, -1]
 for n in num_threads:
-    print(f'Number of Threads: {n} with {kfold_cross_validation} KFolds')
+    print(f'Number of Threads: {n}')
     start = time.time()
-    best_grid_search = GridSearchCV(estimator = XGBClassifier(nthread = n), param_grid = param_grid, cv = kfold_cross_validation, scoring = 'neg_log_loss', n_jobs = n)
-    grid_result = best_grid_search.fit(X, label_encoded_y)
-    
+    grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=n, cv=kfold)   # grid search
+    grid_result = grid_search.fit(X, label_encoded_y)
+
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+      	print("%f (%f) with: %r" % (mean, stdev, param))
+
+    scores = numpy.array(means).reshape(len(learning_rate), len(number_of_trees))
     elapsed = time.time() - start
-    # print(f'Param Grid: {param_grid}')
-    # print(f'N-Estimator: {param_grid['n_estimators']}')
-    # print(f'Learing Rate: {param_grid.get('learning_rate')}')
+
     print(f'Elapsed Duration: {elapsed}')
-    # results.append((num_threads, elapsed))
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 
-
-# print(results)
-# plot results
-# pyplot.plot(num_threads, results)
-# pyplot.ylabel('Speed (seconds)')
-# pyplot.xlabel('Number of Threads')
-# pyplot.title('XGBoost Training Speed vs Number of Threads')
-# pyplot.show()
+scores = numpy.array(means).reshape(len(learning_rate), len(number_of_trees))
+for i, value in enumerate(learning_rate):
+    pyplot.plot(number_of_trees, scores[i], label = 'learning_rate: ' + str(value))
+pyplot.legend()
+pyplot.xlabel('number_of_trees')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('number_of_trees_vs_learning_rate.png')
